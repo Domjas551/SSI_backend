f2320547f34d31b7ef2825751ac73f829f36bae1
Domjas551 <domjas@onet.pl>
2024-10-28 12:34:59 +0100

    Added functions for getting and sending data to frontend.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var mysql=require('mysql2');
 var express = require('express');
+const {log} = require("debug");
 var router = express.Router();
+const app = express();
 
 const pool=mysql.createPool({
   host:'127.0.0.1',
@@ -27,12 +29,27 @@ function select(){
   });
 }
 
+function select2(){
+  return new Promise((resolve, reject)=>{
+    pool.query("Select * from task_type", (err, result)=> {
+      if(err){
+        console.log(err);
+      }else{
+        resolve(result);
+      }
+    });
+  })
+}
+
+
 function insert(){
   pool.query("Insert into task_type values(6,'test')", (err, result)=> {
     if (err) throw err;
     console.log(result);
   });
 }
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -48,5 +65,20 @@ router.get('/s', async(req, res, next)=> {
   select();
   res.render('index', { title: 'Express backend!' });
 });
+
+router.get("/type",(req,res)=>{
+
+  select2().then((data)=>{
+    console.log(data);
+    res.send(data);
+  })
+
+})
+
+router.post("/post",(req,res)=>{
+  console.log(req.body);
+  res.send({message:"mam"});
+})
+
 
 module.exports = router;
