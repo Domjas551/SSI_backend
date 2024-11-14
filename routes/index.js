5e0edd9bc29e3ed778da8b4b68a5b48ce4058d66
Domjas551 <domjas@onet.pl>
2024-11-14 12:30:36 +0100

    Added functionality for adding new users and task types

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,29 @@ function selectUsersData(query="Select user_id, name, surname, email, position, 
   return new Promise((resolve, reject)=>{
     pool.query(query, (err, result)=> {
       if(err){
-        console.log(err.code);
+
+        let message;
+
+        if(err.code=="ECONNREFUSED"){
+          message="Nie można połączyć sie z bazą danych";
+        }else if(err.code=="ER_PARSE_ERROR"){
+          message="Błąd przy próbie pobrania danych";
+        }
+
+        reject(message)
+      }else{
+        resolve(result);
+      }
+    });
+  })
+}
+
+//funkcja do pobierania emaili wszystkich użytkowników
+function getEmails(){
+  return new Promise((resolve, reject)=>{
+    pool.query("Select email from user", (err, result)=> {
+      if(err){
+
         let message;
 
         if(err.code=="ECONNREFUSED"){
@@ -38,7 +60,6 @@ function selectUserData(id){
   return new Promise((resolve, reject)=>{
     pool.query(`Select user_id, name, surname, email, position, is_active from user where user_id=${id}`, (err, result)=> {
       if(err){
-        console.log(err);
 
         let message;
 
@@ -61,7 +82,6 @@ function updateUserData(id,name,surname,position,is_active){
   return new Promise((resolve, reject)=>{
     pool.query(`Update user set name="${name}", surname="${surname}", position="${position}", is_active=${is_active} where user_id=${id}`, (err, result)=> {
       if(err){
-        console.log(err);
 
         let message;
 
@@ -78,11 +98,93 @@ function updateUserData(id,name,surname,position,is_active){
     });
   })
 }
-function insert(){
-  pool.query("Insert into task_type values(null,'test')", (err, result)=> {
-    if (err) throw err;
-    console.log(result);
-  });
+
+//funkcja do resetu hasła użytkownika
+function resetUserPassword(id,password){
+  return new Promise((resolve, reject)=>{
+    pool.query(`Update user set password="${password}" where user_id=${id}`, (err, result)=> {
+      if(err){
+
+        let message;
+
+        if(err.code=="ECONNREFUSED"){
+          message="Nie można połączyć sie z bazą danych";
+        }else if(err.code=="ER_PARSE_ERROR"){
+          message="Błąd przy próbie aktualizacji danych";
+        }
+
+        reject(message)
+      }else{
+        resolve(result);
+      }
+    });
+  })
+}
+
+//funkcja do pobierania danych wszystkich typów danych
+function selectTaskTypesData(query="Select task_type_id, name from task_type"){
+  return new Promise((resolve, reject)=>{
+    pool.query(query, (err, result)=> {
+      if(err){
+
+        let message;
+
+        if(err.code=="ECONNREFUSED"){
+          message="Nie można połączyć sie z bazą danych";
+        }else if(err.code=="ER_PARSE_ERROR"){
+          message="Błąd przy próbie pobrania danych";
+        }
+
+        reject(message)
+      }else{
+        resolve(result);
+      }
+    });
+  })
+}
+
+//funkcja do dodawania nowych użytkowników
+function insertUser(name,surname,password,email,phone,position){
+  return new Promise((resolve, reject)=>{
+    pool.query(`Insert into user values(null, "${name}", "${surname}", "${password}", "${email}", ${phone}, "${position}",1)`, (err, result)=> {
+      if(err){
+        console.log(err)
+        let message;
+
+        if(err.code=="ECONNREFUSED"){
+          message="Nie można połączyć sie z bazą danych";
+        }else if(err.code=="ER_PARSE_ERROR"){
+          message="Błąd przy próbie dodania danych";
+        }
+
+        reject(message)
+      }else{
+        resolve(result);
+      }
+    });
+  })
+}
+
+//funkcja do dodawania nowych typów zadań
+function insertTaskType(query){
+  return new Promise((resolve, reject)=>{
+    pool.query(query, (err, result)=> {
+      if(err){
+        console.log(err)
+        let message;
+
+        if(err.code=="ECONNREFUSED"){
+          message="Nie można połączyć sie z bazą danych";
+        }else if(err.code=="ER_PARSE_ERROR"){
+          message="Błąd przy próbie dodania danych";
+        }
+
+        reject(message)
+      }else{
+        resolve(result);
+      }
+    });
+  })
 }
 
 
@@ -106,8 +208,6 @@ router.get("/users",(req,res)=>{
 
 //Metoda do filtrowania użytkowników
 router.post("/users",(req,res)=>{
-
-  console.log(req.body.query)
 
   if(req.body.query==null){
     selectUsersData().then((data)=>{
@@ -143,23 +243,153 @@ router.put("/user",(req,res)=>{
 
   let alfa=0;
 
-  updateUserData(req.body.id,req.body.name,req.body.surname,req.body.position,req.body.is_active).catch((error)=>{
-    alfa=1;
+  if(req.body.password==undefined){
+
+
+    updateUserData(req.body.id,req.body.name,req.body.surname,req.body.position,req.body.is_active).catch((error)=>{
+      alfa=1;
+      //wysyłanie wiadomości o błędzie na frontend
+      res.send([{ error: error}]);
+    })
+
+    selectUserData(req.body.id).then((data)=>{
+      if(alfa==0){
+        res.send(data)
+      }
+    }).catch((error)=>{
+      if(alfa==0){
+        //wysyłanie wiadomości o błędzie na frontend
+        res.send([{ error: error}]);
+      }
+    })
+  }else{
+    resetUserPassword(req.body.id, req.body.password).catch((error)=>{
+      alfa=1;
+      //wysyłanie wiadomości o błędzie na frontend
+      res.send([{ error: error}]);
+    })
+
+    selectUserData(req.body.id).then((data)=>{
+      if(alfa==0){
+        res.send(data)
+      }
+    }).catch((error)=>{
+      if(alfa==0){
+        //wysyłanie wiadomości o błędzie na frontend
+        res.send([{ error: error}]);
+      }
+    })
+  }
+
+})
+
+//metoda do pobierania emaili użytkowników
+router.get("/admin/userAdd",(req,res)=>{
+
+  getEmails().then((data)=>{
+    res.send(data);
+  }).catch((error)=>{
     //wysyłanie wiadomości o błędzie na frontend
     res.send([{ error: error}]);
   })
 
-  selectUserData(req.body.id).then((data)=>{
+})
+
+router.put("/admin/userAdd",(req,res)=>{
+
+  let alfa = 0;
+
+  insertUser(req.body.name,req.body.surname,req.body.password,req.body.email,req.body.phone,req.body.position,).catch((error) => {
+    alfa = 1;
+    //wysyłanie wiadomości o błędzie na frontend
+    res.send([{error: error}]);
+  })
+
+  getEmails().then((data)=>{
     if(alfa==0){
-      res.send(data)
+      res.send(data);
     }
+
   }).catch((error)=>{
     if(alfa==0){
       //wysyłanie wiadomości o błędzie na frontend
       res.send([{ error: error}]);
     }
   })
+
 })
 
+//metoda do zwracania danych typów zadań
+router.get("/taskTypes",(req,res)=>{
+
+  selectTaskTypesData().then((data)=>{
+    res.send(data);
+  }).catch((error)=>{
+    //wysyłanie wiadomości o błędzie na frontend
+    res.send([{ error: error}]);
+  })
+
+})
+
+//Metoda do filtrowania typów zadań
+router.post("/taskTypes",(req,res)=>{
+
+  if(req.body.query==null){
+    selectTaskTypesData().then((data)=>{
+      res.send(data);
+    }).catch((error)=>{
+      //wysyłanie wiadomości o błędzie na frontend
+      res.send([{ error: error}]);
+    })
+  }else{
+    selectTaskTypesData(req.body.query).then((data)=>{
+      res.send(data);
+    }).catch((error)=>{
+      //wysyłanie wiadomości o błędzie na frontend
+      res.send([{ error: error}]);
+    })
+  }
+
+})
+
+//Metoda do filtrowania typów zadań oraz do dodawania nowych typów
+router.put("/taskTypes",(req,res)=> {
+
+  if (req.body.newType != null){
+    let alfa = 0;
+
+    insertTaskType(req.body.newType).catch((error) => {
+      alfa = 1;
+      //wysyłanie wiadomości o błędzie na frontend
+      res.send([{error: error}]);
+    })
+
+    selectTaskTypesData().then((data) => {
+      if (alfa == 0) {
+        res.send(data)
+      }
+    }).catch((error) => {
+      if (alfa == 0) {
+        //wysyłanie wiadomości o błędzie na frontend
+        res.send([{error: error}]);
+      }
+    })
+  }else if(req.body.query==null){
+    selectTaskTypesData().then((data)=>{
+      res.send(data);
+    }).catch((error)=>{
+      //wysyłanie wiadomości o błędzie na frontend
+      res.send([{ error: error}]);
+    })
+  }else if(req.body.query!=null){
+    selectTaskTypesData(req.body.query).then((data)=>{
+      res.send(data);
+    }).catch((error)=>{
+      //wysyłanie wiadomości o błędzie na frontend
+      res.send([{ error: error}]);
+    })
+  }
+
+})
 
 module.exports = router;
